f648605fd5873e1069edd27220781ef1f0d04af7
yossiabrams <94040130+yossiabrams@users.noreply.github.com>
2023-11-27 13:46:11 +0200

    app.py

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ from google.auth.exceptions import RefreshError
 import requests
 from streamlit_lottie import st_lottie
 from googleapiclient.errors import HttpError
-import time
 
-
-allowed_usernames = ["yossi", "edi","ziv"]
+allowed_usernames = ["yossi", "edi", "ziv"]
 
 st.set_page_config(
     page_title="ארון ציוד",
-    page_icon="https://cdn-icons-png.flaticon.com/128/1013/1013307.png",  # Replace with the URL or local file path of your icon
+    page_icon="https://cdn-icons-png.flaticon.com/128/1013/1013307.png",
+    # Replace with the URL or local file path of your icon
     layout="wide",
     initial_sidebar_state="expanded",  # or "collapsed"
 
@@ -22,14 +21,13 @@ custom_style = """
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
         body {
-            background-color:red;
             margin-top: 0;
             padding: 0;
             font-family: 'Open Sans', sans-serif;
             color: #6A4326; 
             font-family: 'Open Sans', sans-serif;
             direction: rtl !important;
-            
+
         }
         h1 {
             color: #6A4326; /* Custom color for h1 */
@@ -69,33 +67,17 @@ credentials = service_account.Credentials.from_service_account_file(
 # Build the Sheets API client
 service = build("sheets", "v4", credentials=credentials)
 
+
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
 
-last_request_time = 0
-def rate_limit():
-    global last_request_time
-    current_time = time.time()
-    elapsed_time = current_time - last_request_time
-
-    # Set the minimum time between requests (e.g., 60 seconds)
-    min_time_between_requests = 60
-
-    if elapsed_time < min_time_between_requests:
-        # Sleep to ensure the minimum time between requests is met
-        time.sleep(min_time_between_requests - elapsed_time)
-
-    # Update the last request time
-    last_request_time = time.time()
 
 def get_worksheet(product_type):
     try:
-        # Apply rate limiting before making the API call
-        rate_limit()
-
+        # Make a dummy request to force token refresh if needed
         service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
         range_name = f"{product_type}!A:C"
         result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
@@ -105,73 +87,36 @@ def get_worksheet(product_type):
         st.error(f"Error refreshing credentials: {e}")
         raise
     except HttpError as err:
-        if err.resp.status == 429:  # Quota exceeded error
-            st.error(f"Quota exceeded for the Google Sheets API. Waiting for a minute before retrying...")
-            # Display a pop-up with countdown
-            countdown_timer(10)
-            return get_worksheet(product_type)  # Retry the API call
-        else:
-            # Print detailed information about the HTTP error
-            st.error(f"HTTP error occurred: {err.resp.status} - {err._get_reason()}")
-            st.error(f"Error details: {err.content}")
-            if err.resp.status == 403:
-                st.error("Make sure the service account has the necessary permissions to access the spreadsheet.")
-            elif err.resp.status == 404:
-                st.error("Spreadsheet not found. Double-check the spreadsheet_id.")
-            raise
-
-def countdown_timer(duration):
-    with st.spinner(" "):  # Use a space to create an empty spinner
-        for remaining in range(duration, 0, -1):
-            st.spinner(f"Quota exceeded. Waiting for {remaining} seconds...").update()
-            time.sleep(1)
+        # Print detailed information about the HTTP error
+        st.error(f"HTTP error occurred: {err.resp.status} - {err._get_reason()}")
+        st.error(f"Error details: {err.content}")
+        if err.resp.status == 403:
+            st.error("Make sure the service account has the necessary permissions to access the spreadsheet.")
+        elif err.resp.status == 404:
+            st.error("Spreadsheet not found. Double-check the spreadsheet_id.")
+        raise
 
 
 def update_taken_status(product_type, product_name, product_number, username):
     try:
-        # Retrieve the data for the selected product type
-        product_data = get_worksheet(product_type)
-
-        # Check if the product is already taken
-        if any(item[0] == product_name and item[1] == product_number and item[2] == "Taken" for item in product_data if len(item) > 2):
-            st.warning(f"המוצר:  {product_name} {product_number} is already taken.")
-        else:
-            # Find the row index of the product_name and product_number
-            matching_items = [(i, item) for i, item in enumerate(product_data) if item and item[0] == product_name and item[1] == product_number]
-
-            if matching_items:
-                # Update the 'Taken' status and username in column C for the first matching item
-                row_index, _ = matching_items[0]
-                range_name = f"{product_type}!C{row_index + 1}"
-                update_body = {"values": [[f"Taken by {username}"]]}
-                service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_name, body=update_body,
-                                                       valueInputOption="RAW").execute()
-                st.success(f"המוצר:' {product_name} {product_number}' מסומן כ  'נלקח על ידי {username}'")
-            else:
-                st.warning(f"המוצר  {product_name} {product_number} לא נמצא")
+        with st.spinner(f"Processing {product_type}..."):
+            # Rest of your existing code
+            if st.button(f"לקחת {product_type}", key=f"take_{product_type}"):
+                update_taken_status(product_type, selected_product_name, selected_product_number, password)
+                st.button(f"לקחת {product_type}", disabled=True)
 
     except RefreshError as e:
         st.error(f"Error refreshing credentials: {e}")
         raise
 
+
 def update_return_status(product_type, product_name, product_number, username):
     try:
-        # Retrieve the data for the selected product type
-        product_data = get_worksheet(product_type)
-
-        # Find the row index of the product_name and product_number
-        matching_items = [(i, item) for i, item in enumerate(product_data) if item and item[0] == product_name and item[1] == product_number]
-
-        if matching_items:
-            # Update the 'Returned' status and username in column C for the first matching item
-            row_index, _ = matching_items[0]
-            range_name = f"{product_type}!C{row_index + 1}"
-            update_body = {"values": [[f"Returned by {username}"]]}
-            service.spreadsheets().values().update(spreadsheetId=spreadsheet_id, range=range_name, body=update_body,
-                                                   valueInputOption="RAW").execute()
-            st.success(f"המוצר:' {product_name} {product_number}' מסומן כ  'הוחזר על ידי {username}'")
-        else:
-            st.warning(f"המוצר {product_name} {product_number} לא קיים")
+        with st.spinner(f"Processing {product_type}..."):
+            # Rest of your existing code
+            if st.button(f"להחזיר {product_type}", key=f"return_{product_type}"):
+                update_return_status(product_type, selected_product_name, selected_product_number, password)
+                st.button(f"להחזיר {product_type}", disabled=True)
 
     except RefreshError as e:
         st.error(f"Error refreshing credentials: {e}")
@@ -180,6 +125,11 @@ def update_return_status(product_type, product_name, product_number, username):
 
 hide_st_style = """
             <style>
+            ..st-emotion-cache-1s4j85f{}
+            .st-emotion-cache-u2h1j4{gap: 0rem;}
+            .st-emotion-cache-je0s9r{gap: 0rem;}
+            .st-emotion-cache-11jhoak { gap: 0rem;}
+            .st-emotion-cache-yqkwhy { gap: 0rem;}
             .st-emotion-cache-do9jc5 { width: 344.8px;  position: relative; display: flex; flex: 1 1 0%; flex-direction: column; gap: 0rem;}
             .st-emotion-cache-1lzqysu { width: 746.4px; position: relative; display: flex; flex: 1 1 0%; flex-direction: column; gap: 0rem;}
             .st-emotion-cache-z5fcl4 { width: 100%; padding: 0rem 1rem 10rem; min-width: auto;max-width: initial;}
@@ -203,7 +153,6 @@ password = st.text_input(" הכנס את שם המשתמש:", type="password")
 # Product Types
 product_types = ["רחפן", "סוללה", "מטען", "שלט"]
 
-
 # Check if the password is in the list of allowed usernames
 if password in allowed_usernames:
     for product_type in product_types:
@@ -217,7 +166,8 @@ if password in allowed_usernames:
         product_numbers = [item[1] for item in product_data if item]
 
         # Create a dropdown for unique product names
-        selected_product_name = st.selectbox(f"בחר דגם {product_type}:", unique_product_names, key=f"{product_type}_product_name")
+        selected_product_name = st.selectbox(f"בחר דגם {product_type}:", unique_product_names,
+                                             key=f"{product_type}_product_name")
 
         # Create an edit line to select the product number
         selected_product_number = st.text_input(f"בחר מספר {product_type}:", key=f"{product_type}_product_number")
@@ -239,6 +189,6 @@ else:
     st.warning("Incorrect username. Access denied.")
 st.markdown("""
     <div style="margin-top: 20px; padding: 20px;  color: #281100; text-align: center;">
-        <p>פותחחחח על ידי יוסי אברמס עבור 'חשיפה ודיגיטל' </p>
+        <p>פותח על ידי יוסי אברמס עבור 'חשיפה ודיגיטל' </p>
     </div>
 """, unsafe_allow_html=True)
